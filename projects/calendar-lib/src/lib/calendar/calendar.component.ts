d5c7b74e78aeb8d2f34c7a82180f14448793d44e
Tom Lambert <tom.lambert@picosmos.de>
2021-01-19 15:06:02 +0100

    removed console.log

--- projects/calendar-lib/src/lib/calendar/calendar.component.ts
+++ projects/calendar-lib/src/lib/calendar/calendar.component.ts
@@ -53,7 +53,6 @@ export class CalendarComponent implements OnInit {
   public drawLineInCompactLayout: (day: number) => boolean =
     (x) => {
       let firstMonth = new Date(this.year, this.month - 1, 1, 12, 0, 0, 0).getDay();
-      console.log({ x, firstMonth, a: (firstMonth - 1 + x) })
       return (firstMonth - 1 + x) % 7 == 0;
     };
 
